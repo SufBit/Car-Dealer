f64c58cdaea503cdf64240b2b44b7f5aba058a61
SufBit <sufarshad.99@gmail.com>
2024-08-11 22:49:00 -0500

    linting issue

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -20,8 +19,6 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
     
-
-
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
